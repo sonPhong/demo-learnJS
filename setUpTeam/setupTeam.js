bc7c106de4c58cd4d1476feee159bfb7a857b295
Pieforax25 <lamhungsonphong155@gmail.com>
2025-05-07 17:51:09 +0700

    fix feature with AI

--- setUpTeam/setupTeam.js
+++ setUpTeam/setupTeam.js
@@ -32,38 +32,32 @@ console.log(`Cách chọn team 2: ${team2.length} ==> Tổng: ${team1.length + t
 //---------------------Request 2 chọn 3 người thành 1 đội, ràng buộc điều kiện---------------------
 // điều kiện: - cặp bài trùng HLV muốn ghép chung <=> có cặp không thể ghép chung  ==> thêm ràng buộc điều kiện đầu có thể thay đổi
 
-// Tạo cặp bài trùng và cặp đối đầu
-// const AllTeam = [];
+const teamPair = ["CL1-CL3", "DB3-DB4"]; // Các cặp bắt buộc đi chung
+const teamOppo = ["CL3-DB1", "CL5-DB2"]; // Các cặp không thể chơi cùng nhau
 
-// for (i = 0; i <= 40; i++) {
-//     AllTeam.push(i + "");
-// }
+let validTeams = [];
 
-// console.log(AllTeam);
+// Duyệt tất cả các khả năng để chọn đội
+for (let core of CL) { // Chọn 1 người từ đội nòng cốt
+    for (let reserve of DB) { // Chọn 1 người từ đội dự bị
+        let team = [NC[0], core, reserve];
 
-// // const readline = require("readline");
+        // Kiểm tra các cặp bắt buộc
+        let hasMandatoryPair = teamOppo.some(pair =>
+            team.includes(pair.split("-")[0]) && team.includes(pair.split("-")[1])
+        );
 
-// // const rl = readline.createInterface({
-// //     input: process.stdin, //read
-// //     output: process.stdout // write
-// // });
+        // Kiểm tra các cặp bị cấm
+        let hasForbiddenPair = teamPair.some(pair =>
+            team.includes(pair.split("-")[0]) && team.includes(pair.split("-")[1])
+        );
 
-// // rl.question("Nhập số bạn muốn là cặp bài trùng (ex: 12-22): ", function (answer) {
-// //     console.log("Lựa chọn của bạn là: , " + answer + "!");
-// // });
+        // Chỉ thêm vào danh sách nếu hợp lệ
+        if (hasMandatoryPair && !hasForbiddenPair) {
+            validTeams.push(team);
+        }
+    }
+}
 
-// // team chọn 3 người trong 40
-// const team3 = [];
-
-// const coupPair = ["20-11", "30-3", "21-9"];
-// const coupOppo = ["2-3", "7-10", "2-19"];
-
-// for (let i = 0; i < AllTeam.length; i++) {
-//     for (let j = i + 1; j < AllTeam.length; j++) {
-//         for (let k = j + 1; k < AllTeam.length; k++) {
-//             team3.push([AllTeam[i], AllTeam[j], AllTeam[k]]);
-//         }
-//     }
-// }
-
-// console.log(team3);
+console.log(validTeams);
+console.log("Tổng số đội hợp lệ:", validTeams.length);
